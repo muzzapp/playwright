43f94aa5dbb9648ac25c056415da570c86659ab9
Aoife McClean <aoife.mcclean@muzz.com>
2025-06-27 11:43:07 +0100

    play store test

--- tests/Muzz Blog/muzz-blog.spec.ts
+++ tests/Muzz Blog/muzz-blog.spec.ts
@@ -7,3 +7,11 @@ test('I can view the app in the app store via the Muzz website', async ({ page }
     await page.getByRole('link', { name: 'App Store icon' }).click();
     const page1 = await page1Promise;
   });
+
+  test('I can view the app in the play store via the Muzz website', async ({ page }) => {
+    await page.goto('https://muzz.com/en-GB/');
+    await page.getByRole('link', { name: 'Download', exact: true }).click();
+    const page1Promise = page.waitForEvent('popup');
+    await page.getByRole('link', { name: 'Google Play icon' }).click();
+    const page1 = await page1Promise;
+  });
