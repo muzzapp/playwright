c48fbaf436cc28ed12aae2fd2a7c8f4cea8ed4be
Aoife McClean <aoife.mcclean@muzz.com>
2025-06-27 12:01:40 +0100

    career apply test

--- tests/Muzz Blog/muzz-blog.spec.ts
+++ tests/Muzz Blog/muzz-blog.spec.ts
@@ -15,3 +15,14 @@ test('I can view the app in the app store via the Muzz website', async ({ page }
     await page.getByRole('link', { name: 'Google Play icon' }).click();
     const page1 = await page1Promise;
   });
+
+  test('I can apply for an open role via the Muzz website', async ({ page }) => {
+    await page.goto('https://muzz.com/en-GB/');
+    await page.locator('#navigation').getByRole('link', { name: 'Careers' }).click();
+    await page.getByRole('link', { name: 'See Open Roles' }).click();
+    await page.getByRole('link', { name: 'Senior iOS Engineer Full-time' }).click();
+    await page.locator('div').filter({ hasText: 'Apply Now' }).getByRole('link').click();
+    const page1Promise = page.waitForEvent('popup');
+    await page.getByRole('button', { name: 'Apply Now' }).getByRole('link').click();
+    const page1 = await page1Promise;
+  });
